84e33d799a0ae41153513f9cb3fcddf5366ac842
Anton Dubkov <dubkov@gmail.com>
2016-12-14 15:31:17 +0700

    яЭ

--- CLOSE.lua
+++ CLOSE.lua
@@ -2,18 +2,18 @@ local INSTRUMENT = "SiZ6"
 local ACCOUNT = "SPBFUT00LLD"
 local CLIENT_CODE = "SPBFUT00LLD" -- код клиента
 
-local CLOSE_TIME = {hour=15, min=20} -- врем§ закрыти§
-local QUANTITY = 1; -- количество дл§ закрыти§
+local CLOSE_TIME = {hour=15, min=20} -- время закрытия
+local QUANTITY = 1; -- количество для закрытия
 local OPERATION = "B"; -- "B" -- купить, "S" -- продать
 
 local BUFFER = 100 -- спред/проскальзывание
 
 
--- вызываетс§ квиком при запуске робота
+-- вызывается квиком при запуске робота
 function OnInit()
     -- инициализируем data source
     DS = CreateDataSource("SPBFUT", INSTRUMENT, INTERVAL_M5)
-    -- подпишемс§ на обновление цены INSTRUMENT (SiZ6)
+    -- подпишемся на обновление цены INSTRUMENT (SiZ6)
     DS:SetUpdateCallback(NewPrice)
     PrintDbgStr("Скрипт закрытия запущен! ".. OPERATION .. " " .. tostring(QUANTITY) .. " в " .. CLOSE_TIME.hour .. ":" .. CLOSE_TIME.min)
 end
@@ -23,13 +23,13 @@ end
 function doClose(curPrice)
     PrintDbgStr("Закрываемся!")
 
-    local price; -- цена за§вки
+    local price; -- цена заявки
     if OPERATION == "B" then
-        price = curPrice + BUFFER -- цена за§вки на покупку
+        price = curPrice + BUFFER -- цена заявки на покупку
     else
-        price = curPrice - BUFFER -- цена за§вки на продажу
+        price = curPrice - BUFFER -- цена заявки на продажу
     end
-    -- за§вка
+    -- заявка
     local order = {
         ["TRANS_ID"]    = "1",
         ["ACTION"]      = "NEW_ORDER",
@@ -45,7 +45,7 @@ function doClose(curPrice)
 
 end
 
-function NewPrice(i)  -- функци§ вызываетс§ при каждом обновлении цены
+function NewPrice(i)  -- функция вызывается при каждом обновлении цены
     local curPrice = DS:C(i)
     local dt = DS:T(i)
     if dt.hour >= CLOSE_TIME.hour and dt.min >= CLOSE_TIME.min then
@@ -58,7 +58,7 @@ end
 
 Run = true
 
--- основна§ функци§ (нужна чтобы робот работал)
+-- основная функция (нужна чтобы робот работал)
 function main()
     while Run do
         sleep(200)
