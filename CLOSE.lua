aecd7ac9a6f18b5219f534becdd2d21dfa9c130d
Anton Dubkov <dubkov@gmail.com>
2017-01-09 17:49:14 +0700

    Fixed to CLOSE script

--- CLOSE.lua
+++ CLOSE.lua
@@ -1,21 +1,22 @@
-local INSTRUMENT = "SiZ6"
-local ACCOUNT = "SPBFUT00LLD"
-local CLIENT_CODE = "SPBFUT00LLD" -- код клиента
+require "settings" -- берем инструмент, код клиента и спрэд оттуда
 
 local CLOSE_TIME = {hour=15, min=20} -- время закрытия
 local QUANTITY = 1; -- количество для закрытия
 local OPERATION = "B"; -- "B" -- купить, "S" -- продать
 
-local BUFFER = 100 -- спред/проскальзывание
+local trade_date = {}
 
 
 -- вызывается квиком при запуске робота
 function OnInit()
+
+	-- проверка текущей даты
+	trade_date = os.date("*t", os.time())
     -- инициализируем data source
-    DS = CreateDataSource("SPBFUT", INSTRUMENT, INTERVAL_M5)
+    DS = CreateDataSource("SPBFUT", INSTRUMENT, INTERVAL_M1)
     -- подпишемся на обновление цены INSTRUMENT (SiZ6)
     DS:SetUpdateCallback(NewPrice)
-    PrintDbgStr("Cкрипт закрытия запущен! ".. OPERATION .. " " .. tostring(QUANTITY) .. " в " .. CLOSE_TIME.hour .. ":" .. CLOSE_TIME.min)
+    PrintDbgStr("Cкрипт закрытия запущен!".. trade_date.day .." ".. OPERATION .. " " .. tostring(QUANTITY) .. " в " .. CLOSE_TIME.hour .. ":" .. CLOSE_TIME.min)
 end
 
 
@@ -48,10 +49,10 @@ end
 function NewPrice(i)  -- функция вызывается при каждом обновлении цены
     local curPrice = DS:C(i)
     local dt = DS:T(i)
-    if dt.hour > CLOSE_TIME.hour or (dt.hour == CLOSE_TIME.hour and dt.min >= CLOSE_TIME.min) then
+    if Run and dt.day == trade_date.day and (dt.hour > CLOSE_TIME.hour or (dt.hour == CLOSE_TIME.hour and dt.min >= CLOSE_TIME.min)) then
+        Run = false -- завершение работы!!!
         PrintDbgStr("Время закрытия наступило!")
         doClose(curPrice)
-        Run = false -- завершение работы!!!
     end
 end
 
@@ -61,7 +62,7 @@ Run = true
 -- основная функция (нужна чтобы робот работал)
 function main()
     while Run do
-        sleep(200)
+        sleep(1000)
     end
 end
 
